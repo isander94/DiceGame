6e1d8b383ccd1403d2478ac676f0134c50be0932
Leon Kardum <leon.kardum0040@stud.hkr.se>
2025-09-24 08:41:15 +0200

    wrote class for dice

---
+++ src/dice/dice.py
@@ -0,0 +1,17 @@
+"""Class representing a die that can be rolled."""
+
+import random
+
+
+class Dice:
+    """Create a dice class with a low and high value."""
+
+    def __init__(self, low, high):
+        """Initialize with low and high number."""
+        self.low = low
+        self.high = high
+
+    def roll_dice(self):
+        """Roll the dice, returning a number between low and high."""
+        random_num = random.randint(self.low, self.high)
+        return random_num
