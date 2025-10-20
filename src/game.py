c4bd1a305231616a5fa0019de606e510186e8cf2
isander94 <isaac.andersson0015@stud.hkr.se>
2025-10-20 13:23:13 +0200

    Restructured game logic and functions

--- src/game.py
+++ src/game.py
@@ -8,90 +8,86 @@ class Game:
         """Initialize a game instance"""
         self.game_is_active = True
 
-    def player_versus_player(self):
+    def player_versus_player(self, players):
         """Method handling the game mode for two human players"""
-        print("Player versus Player mode started!")
-
-        # The two players shall have a die with 6 faces
-        low = 1
-        high = 6
-
-        # Ask the two players to enter their name and create player objects
-        name = input("Player 1, please enter your name: ")
-        player1 = Player(name, low, high)
+        self.round_score = 0
+        self.round_is_active = True
+        self.show_commands()
         
-        name = input("Player 2, please enter your name: ")
-        player2 = Player(name, low, high)
-
-        print(f"Welcome {player1.get_name()} and {player2.get_name()} - Prepare to roll!")
-
-        # Enter a loop that alternates the control between player 1 and player 2
+        # Main game loop, continues until there is a winner or someone quits       
         while self.game_is_active:
-            # Perform one or multiple moves            
-            self.player_choice(player1)
-            # Check if the player has won after one or multiple moves
-            if self.check_if_won(player1) or not self.game_is_active:
-                break
-            
-            self.player_choice(player2)
-            if self.check_if_won(player2) or not self.game_is_active:
-                break
-
-    def player_choice(self, player):
-        """Reads the player input and executes the corresponding command"""
-        round_score = 0
-        round_is_active = True
-        
-        print(f"{player.get_name()}'s turn!")
-        
-        while round_is_active:
-            player_input = input("--> ")
-            
-            # Roll
-            if player_input.lower() == "roll":
-                roll = player.roll_dice()
-                print(f"{player.get_name()} rolls {roll}!")
+            self.round_is_active = True
+            # Alternate between the players
+            if self.game_is_active:
+                print(f"{players[0].get_name()}'s turn!")
+            while self.round_is_active and self.game_is_active:
+                move = input("---> ")
+                self.player_choice(players[0], move)
+                if self.check_if_won(players[0]):
+                    self.game_is_active = False
                 
-                # If the player rolls 1, the points for the round are lost
-                if roll == 1:
-                    print(f"{player.get_name()} lost all points for this turn!")
-                    round_score = 0
-                    round_is_active = False
+            self.round_score = 0
+            self.round_is_active = True
+            
+            if self.game_is_active:
+                print(f"{players[1].get_name()}'s turn!")
+            while self.round_is_active and self.game_is_active:
+                move = input("---> ")
+                self.player_choice(players[1], move)
+                if self.check_if_won(players[1]):
+                    self.game_is_active = False
+                
+        # Return true when the game is finished
+        return True
+
+
+    def player_choice(self, player, choice):
+        """Take a player object and a command and executes the corresponding game move"""
                     
-                # If the player rolls above 1, the points are added to this rounds score
-                else:
-                    round_score += roll
-                    print(f"{player.get_name()} now has {player.get_score() + round_score} points!")
-
-            # Hold
-            elif player_input.lower() == "hold":
-                # Add the round score to the player object - These points are now kept
-                player.add_score(round_score)
-                print(f"{player.get_name()} has secured {player.get_score()} points!")
-                round_score = 0
-                round_is_active = False
+        # Roll dice
+        if choice.lower() == "roll":
+            self.execute_roll(player)
                 
-            # Quit
-            elif player_input.lower() == "quit":
-                self.game_is_active = False
-                round_is_active = False
-                
-            # Cheat
-            elif player_input.lower() == "cheat":
-                self.cheat(player)
-                round_is_active = False
-                
-            # Change name
-            elif player_input.lower() == "changename":
-                old_name = player.get_name()
-                new_name = input("Enter a new name --> ")
-                player.change_name(new_name)
-                print(f"{old_name} has changed his/her name to {player.get_name()}")
-                
-            # Show commands
-            elif player_input.lower() == "commands":
-                self.show_commands()
+        # Handle hold
+        elif choice.lower() == "hold":
+            player.add_score(self.round_score)
+            self.round_is_active = False
             
+        # Quit game session
+        elif choice.lower() == "quit":
+            self.game_is_active = False
+            
+        # Perform a cheat
+        elif choice.lower() == "cheat":
+            self.cheat(player)
+            return True
+            
+        # Change name of the player
+        elif choice.lower() == "changename":
+            new_name = input("Enter new name: ")
+            player.change_name(new_name)
+            
+        # Show game commands
+        elif choice.lower() == "commands":
+            self.show_commands()
+            
+    def execute_roll(self, player):
+        """Rolls the player dice. If 1, the points are lost. If not, the points are added to the round"""
+        roll = player.roll_dice()
+        print(f"{player.get_name()} rolls {roll}!")
+                
+        # If the player rolls 1, the points for the round are lost
+        if roll == 1:
+            print(f"{player.get_name()} lost all points for this turn!")
+            self.round_score = 0
+            self.round_is_active = False
+                    
+        # If the player rolls above 1, the points are added to this rounds score
+        else:
+            self.round_score += roll
+            print(f"{player.get_name()} now has {player.get_score() + self.round_score} points!")
+        
+    
     def check_if_won(self, player):
         """A simple function that checks if a player has won the game"""
         if player.get_score() >= 100:
@@ -101,6 +97,7 @@ class Game:
             highscore = Highscore()
             highscore.add_score(player.get_name(), player.get_score())
             return True
+        return False
         
     def cheat(self, player):
         """A small function that adds 100 points to the player, thus ending the game with a victory"""
